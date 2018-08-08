df10a18cbaf389851f069895edc843909d6962af
toonn <toonn@toonn.io>
2018-08-08 19:55:24 +0200

    Add gamepad support
    The extension now works for PS3 gamepads. All the buttons are supported
    however, L2 and R2 lack their analog-ness because most gamepads don't
    support that physically.
    
    Add a reporter block for the direction of the analog sticks. Very useful
    when you  want to use the sticks to control the direction of a sprite.
    The alternative is complicated calculations involving arctangents on the
    X and Y coordinates of the sticks.
    
    The coordinates of the sticks are normalized between -100 and 100.
    Deadzones are implemented according to "The Right Way" from
    [this article][Doing Thumbstick Dead Zones Right].
    
    Rumble functionality has a command block but is still missing.
    
    [Doing Thumbstick Dead Zones Right]: http://www.third-helix.com/2013/04/12/doing-thumbstick-dead-zones-right.html

--- katana.py
+++ katana.py
@@ -1,38 +1,110 @@
 #! /usr/bin/env python3
-from inputs import get_key
+from inputs import get_gamepad
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
-import random
+from math import atan2, degrees, hypot, copysign
 
-poll_values = {}
+poll_values = { 'joystick/L/X' : 0
+              , 'joystick/L/Y' : 0
+              , 'joystick/R/X' : 0
+              , 'joystick/R/Y' : 0
+              , 'angle/L' : 90
+              , 'angle/R' : 90
+              }
 
-buttonMap = { 'KEY_E'     : 'button/\u25B2'
-            , 'KEY_D'     : 'button/\u25BC'
-            , 'KEY_S'     : 'button/\u25C0'
-            , 'KEY_F'     : 'button/\u25B6'
-            , 'KEY_K'     : 'button/\u2A2F'
-            , 'KEY_L'     : 'button/\u25CB'
-            , 'KEY_I'     : 'button/\u25B3'
-            , 'KEY_J'     : 'button/\u25A1'
-            , 'KEY_R'     : 'button/L1'
-            , 'KEY_T'     : 'button/L2'
-            , 'KEY_G'     : 'button/L3'
-            , 'KEY_U'     : 'button/R1'
-            , 'KEY_Y'     : 'button/R2'
-            , 'KEY_H'     : 'button/R3'
-            , 'KEY_V'     : 'joystick/L/X'
-            , 'KEY_N'     : 'joystick/R/X'
-            , 'KEY_C'     : 'button/\u25AC'
-            , 'KEY_COMMA' : 'button/\u25BA'
+buttonMap = { 'KEY_E'          : 'button/\u25B2'
+            , 'KEY_D'          : 'button/\u25BC'
+            , 'KEY_S'          : 'button/\u25C0'
+            , 'KEY_F'          : 'button/\u25B6'
+            , 'KEY_K'          : 'button/\u2A2F'
+            , 'KEY_L'          : 'button/\u25CB'
+            , 'KEY_I'          : 'button/\u25B3'
+            , 'KEY_J'          : 'button/\u25A1'
+            , 'KEY_R'          : 'button/L1'
+            , 'KEY_T'          : 'button/L2'
+            , 'KEY_G'          : 'button/L3'
+            , 'KEY_U'          : 'button/R1'
+            , 'KEY_Y'          : 'button/R2'
+            , 'KEY_H'          : 'button/R3'
+            , 'KEY_V'          : 'joystick/L/X'
+            , 'KEY_N'          : 'joystick/R/X'
+            , 'KEY_C'          : 'button/\u25AC'
+            , 'KEY_COMMA'      : 'button/\u25BA'
+            , 'BTN_DPAD_UP'    : 'button/\u25B2'
+            , 'BTN_DPAD_DOWN'  : 'button/\u25BC'
+            , 'BTN_DPAD_LEFT'  : 'button/\u25C0'
+            , 'BTN_DPAD_RIGHT' : 'button/\u25B6'
+            , 'BTN_SOUTH'      : 'button/\u2A2F'
+            , 'BTN_EAST'       : 'button/\u25CB'
+            , 'BTN_NORTH'      : 'button/\u25B3'
+            , 'BTN_WEST'       : 'button/\u25A1'
+            , 'BTN_TL'         : 'button/L1'
+            , 'BTN_TL2'        : 'button/L2'
+            , 'BTN_THUMBL'     : 'button/L3'
+            , 'BTN_TR'         : 'button/R1'
+            , 'BTN_TR2'        : 'button/R2'
+            , 'BTN_THUMBR'     : 'button/R3'
+            , 'ABS_X'          : 'joystick/L/X'
+            , 'ABS_Y'          : 'joystick/L/Y'
+            , 'ABS_RX'         : 'joystick/R/X'
+            , 'ABS_RY'         : 'joystick/R/Y'
+            , 'BTN_SELECT'     : 'button/\u25AC'
+            , 'BTN_START'      : 'button/\u25BA'
+            , 'BTN_MODE'       : 'analog'
             }
+
+def joystick(stick):
+    return stick[9]
+
+def magnitude(key_x, key_y):
+    return hypot(poll_values[key_x], poll_values[key_y])
+
+def deadzone(stick, val):
+    dz = 13
+    mag = abs(val)
+    if mag <= dz:
+        stickX = 'joystick/{0}/X'.format(joystick(stick))
+        stickY = 'joystick/{0}/Y'.format(joystick(stick))
+        if magnitude(stickX, stickY) <= dz:
+            poll_values[stickX] = 0
+            poll_values[stickY] = 0
+        return 0
+    else:
+        return round(copysign(100 * (mag - dz)/(127.5 - dz), val))
+
+def compass(stick):
+    if 'L' in stick:
+        x = poll_values['joystick/L/X']
+        y = poll_values['joystick/L/Y']
+    else:
+        x = poll_values['joystick/R/X']
+        y = poll_values['joystick/R/Y']
+    if x == 0 and y == 0:
+        if 'L' in stick:
+            compass = poll_values['angle/L']
+        else:
+            compass = poll_values['angle/R']
+    else:
+        compass = round(180 - degrees(atan2(y,x)))
+    return compass
 
 def kathread():
     global poll_values
     while True:
-        es = get_key()
+        es = get_gamepad()
         for e in es:
             if e.ev_type == 'Key':
-                poll_values[buttonMap[e.code]] = ['false','true','true'][e.state]
+                poll_values[buttonMap[e.code]] = ['false', 'true', 'true'
+                                                 ][ e.state]
+            elif e.ev_type == 'Absolute':
+                if 'Y' in e.code:
+                    e.state = 128 - e.state
+                else:
+                    e.state = e.state - 127
+                poll_values[buttonMap[e.code]] = deadzone(buttonMap[e.code],
+                    e.state)
+                poll_values['angle/'+joystick(buttonMap[e.code])] = compass(
+                    buttonMap[e.code])
 
 class KatanaHandler(BaseHTTPRequestHandler):
     
